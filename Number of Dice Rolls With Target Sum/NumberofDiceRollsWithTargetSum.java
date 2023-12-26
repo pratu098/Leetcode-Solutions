d616811db8f1a91167cb83ebd4730beadff17fcf
Pratiksha Aggarwal <43148806+pratu098@users.noreply.github.com>
2023-12-26 19:40:06 +0530

    Create NumberofDiceRollsWithTargetSum.java

---
+++ Number of Dice Rolls With Target Sum/NumberofDiceRollsWithTargetSum.java
@@ -0,0 +1,26 @@
+class Solution {
+    int MOD = 1000000007;
+    
+    public int numRollsToTarget(int n, int k, int target) {
+        if (n == 0 && target == 0) 
+            return 1;
+        if (target < n || n * k < target) 
+            return 0;
+        
+        int[][] dp = new int[n + 1][target + 1];
+        
+        for (int i = 1; i <= k && i <= target; i++) {
+            dp[1][i] = 1;
+        }
+        
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j <= target; j++) {
+                for (int x = 1; x <= k && x < j; x++) {
+                    dp[i][j] = (dp[i][j] + dp[i - 1][j - x]) % MOD;
+                }
+            }
+        }
+        
+        return dp[n][target];
+    }
+}
